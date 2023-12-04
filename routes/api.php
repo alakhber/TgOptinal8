689b4235eca9d78c6a8ad09811865ca2276114e6
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-12-04 12:23:37 +0400

    asdad

--- routes/api.php
+++ routes/api.php
@@ -23,24 +23,27 @@ Route::match(['get', 'post'], '/telegram', function () {
     $botToken = "6120014489:AAGsUveht3tNIPIQsrsKYkOxIXhs1-zv8Sg";
     $website = "https://api.telegram.org/bot" . $botToken;
     $chatId = "-1001527566751";  // Receiver Chat ID
-    if(isset($update['update_id'])){
-
+    if (isset($update['update_id'])) {
         file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ Cango Data" . json_encode([$update]));
+
+
+
+        $token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
+        $chat_id = "1175133970";
+        $button1 = ["text" => "Sifariş Təhvil Verildi", "callback_data" => "tehvil verildi"];
+        $button2 = ["text" => "Təhvil Verilə Bilmədi", "callback_data" => "tehvil verilmedi"];
+        $keyboard = [
+            "inline_keyboard" => [
+                [$button1, $button2],
+            ],
+        ];
+        $url = "https://api.telegram.org/bot$token/sendMessage";
+        $data = [
+            "chat_id" => $chat_id,
+            "text" => "Lütfen bir seçenek seçin:",
+            "reply_to_message_id" => $update['update_id'],
+            "reply_markup" => json_encode($keyboard),
+        ];
+        $response = file_get_contents($url . '?' . http_build_query($data));
     }
-    // $token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
-    // $chat_id = "1175133970";
-    // $button1 = ["text" => "Sifariş Təhvil Verildi", "callback_data" => "tehvil verildi"];
-    // $button2 = ["text" => "Təhvil Verilə Bilmədi", "callback_data" => "tehvil verilmedi"];
-    // $keyboard = [
-    //     "inline_keyboard" => [
-    //         [$button1, $button2],
-    //     ],
-    // ];
-    // $url = "https://api.telegram.org/bot$token/sendMessage";
-    // $data = [
-    //     "chat_id" => $chat_id,
-    //     "text" => "Lütfen bir seçenek seçin:",
-    //     "reply_markup" => json_encode($keyboard),
-    // ];
-    // $response = file_get_contents($url . '?' . http_build_query($data));
 });
