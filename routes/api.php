e416a0203cc5ea0a570e679fd1410391ffd832fb
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-12-04 15:52:48 +0400

    asdas

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,22 @@ Route::match(['get', 'post'], '/telegram', function () {
     if (isset($update)) {
 
 
-        // $token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
+      
+
+        file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ Cango Data:" . json_encode($update));
+            // file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ URL:" .);
+        
+
+
+
+
+
+    }
+    http_response_code(200);
+});
+
+Route::get('a',function(){
+      // $token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
         // $button1 = ["text" => "Sifariş Təhvil Verildi", "callback_data" => "tehvil verildi"];
         // $button2 = ["text" => "Təhvil Verilə Bilmədi", "callback_data" => "tehvil verilmedi"];
         // $keyboard = [
@@ -40,21 +55,6 @@ Route::match(['get', 'post'], '/telegram', function () {
         // $url .= "&reply_to_message_id=".$update['message']['message_id'];
         // $url .= "&reply_markup=".json_encode($keyboard);
         // file_get_contents($url);
-
-        file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ Cango Data:" . json_encode($update));
-        
-            // file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ URL:" .);
-        
-
-
-
-
-
-    }
-    http_response_code(200);
-});
-
-Route::get('a',function(){
     $a = file_get_contents('https://api.telegram.org/bot6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA/getUpdates');
     $a=json_decode($a)->result;
     dd($a);
