439e20a42ae335c2ef02908d34f65fa1676198ce
Alakhber Nakhiyev <nakhiyev.alakhber@gmail.com>
2023-12-04 11:44:58 +0400

    dasdsa

--- routes/api.php
+++ routes/api.php
@@ -20,51 +20,38 @@ Route::match(['get', 'post'], '/telegram', function () {
     $update = json_decode($content, true);
 
     // Mesajı işle
-        $botToken = "6120014489:AAGsUveht3tNIPIQsrsKYkOxIXhs1-zv8Sg";
-        $website = "https://api.telegram.org/bot" . $botToken;
-        $chatId = "-1001527566751";  // Receiver Chat ID
-        file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ 166karqo kargoAz:send" . json_encode([$update]));
-});
+    $botToken = "6120014489:AAGsUveht3tNIPIQsrsKYkOxIXhs1-zv8Sg";
+    $website = "https://api.telegram.org/bot" . $botToken;
+    $chatId = "-1001527566751";  // Receiver Chat ID
+    file_get_contents($website . "/sendMessage?chat_id=" . $chatId . "&text= ‼️ 166karqo kargoAz:send" . json_encode([$update]));
+    if (isset($update)) {
 
-/*
-// Telegram botunuzun token'ını buraya ekleyin
-$token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
-    
-// Hedef kullanıcının chat ID'sini buraya ekleyin
-$chat_id = "1175133970";
+        // Telegram botunuzun token'ını buraya ekleyin
+        $token = "6861442315:AAEWIjrctfvW5w_dBeaF7wykJOABiIVHDSA";
 
-// İki adet düğme oluşturun
-$button1 = ["text" => "Button 1", "callback_data" => "button1"];
-$button2 = ["text" => "Button 2", "callback_data" => "button2"];
+        // Hedef kullanıcının chat ID'sini buraya ekleyin
+        $chat_id = "1175133970";
 
-// Düğmeleri içeren bir dizi oluşturun
-$keyboard = [
-    "inline_keyboard" => [
-        [$button1, $button2],
-    ],
-];
+        // İki adet düğme oluşturun
+        $button1 = ["text" => "Button 1", "callback_data" => "button1"];
+        $button2 = ["text" => "Button 2", "callback_data" => "button2"];
 
-// Düğmeleri gönderen isteği oluşturun
-$url = "https://api.telegram.org/bot$token/sendMessage";
-$data = [
-    "chat_id" => $chat_id,
-    "text" => "Lütfen bir seçenek seçin:",
-    "reply_markup" => json_encode($keyboard),
-];
+        // Düğmeleri içeren bir dizi oluşturun
+        $keyboard = [
+            "inline_keyboard" => [
+                [$button1, $button2],
+            ],
+        ];
 
-// İstek gönderin
-$response = file_get_contents($url . '?' . http_build_query($data));
+        // Düğmeleri gönderen isteği oluşturun
+        $url = "https://api.telegram.org/bot$token/sendMessage";
+        $data = [
+            "chat_id" => $chat_id,
+            "text" => "Lütfen bir seçenek seçin:",
+            "reply_markup" => json_encode($keyboard),
+        ];
 
-// Yanıtı kontrol edin
-if ($response) {
-    $result = json_decode($response, true);
-    if ($result['ok']) {
-        echo "Düğmeler başarıyla gönderildi.";
-    } else {
-        echo "Hata: " . $result['description'];
+        // İstek gönderin
+        $response = file_get_contents($url . '?' . http_build_query($data));
     }
-} else {
-    echo "Telegram API'ye istek gönderilemedi.";
-}
-
-*/
+});
